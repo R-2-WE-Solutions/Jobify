4534f647b5140a63fb1266f0b08fffe24e55c759
Zenith <waedatwi10@gmail.com>
2026-04-26 22:00:43 +0300

    fix again??????????????????

--- frontend/frontend.client/src/pages/admin/AdminRecruiters.tsx
+++ frontend/frontend.client/src/pages/admin/AdminRecruiters.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CheckCircle, XCircle, Clock, Mail, Search } from "lucide-react";
 
 interface Recruiter {
